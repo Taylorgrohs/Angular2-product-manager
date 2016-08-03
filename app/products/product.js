e24a5ae9f319728051e5fff9fdf3a72570faa3ff
Taylor Grohs <taylorgrohs@gmail.com>
2016-08-03 13:00:45 -0600

    yeap

---
+++ app/products/product.js
@@ -0,0 +1,28 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Product;
+    return {
+        setters:[],
+        execute: function() {
+            Product = (function () {
+                function Product(productId, productName, productCode, releaseDate, price, description, starRating, imageUrl) {
+                    this.productId = productId;
+                    this.productName = productName;
+                    this.productCode = productCode;
+                    this.releaseDate = releaseDate;
+                    this.price = price;
+                    this.description = description;
+                    this.starRating = starRating;
+                    this.imageUrl = imageUrl;
+                }
+                Product.prototype.calculateDiscount = function (percent) {
+                    return this.price - (this.price * percent / 100);
+                };
+                return Product;
+            }());
+            exports_1("Product", Product);
+        }
+    }
+});
+//# sourceMappingURL=product.js.map
