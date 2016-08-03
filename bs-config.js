b6758549c1f8256767c5361c465c609270d8b064
Taylor Grohs <taylorgrohs@gmail.com>
2016-08-03 15:27:32 -0600

    breaking stuff

--- bs-config.js
+++ bs-config.js
@@ -1,3 +1,12 @@
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
